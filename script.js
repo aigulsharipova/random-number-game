e33b64b424790b91df1e9d8fcfd3a58d112ccc23
Aigul <sharipova.aig@gmail.com>
2022-07-18 20:57:41 -0400

    const answer

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let myNumber = document.querySelector('#guess');
 let button = document.querySelector('#btn');
+const answer = Math.floor(Math.random()*20)+1;
 
 button.addEventListener('click', play);
 function play() {
@@ -11,4 +12,12 @@ if (userNumber < 1 || userNumber > 20) {
     text: 'Select number from 1 to 20',
   })
 }
+else if (isNaN(userNumber)) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...! Something went wrong!',
+    text: 'Write the number',
+  })
+}
+
 }
