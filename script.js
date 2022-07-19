adfef60d6995a018accd1182d6f934a6790ed1ba
Aigul <sharipova.aig@gmail.com>
2022-07-18 20:51:56 -0400

    function

--- script.js
+++ script.js
@@ -4,4 +4,11 @@ let button = document.querySelector('#btn');
 button.addEventListener('click', play);
 function play() {
 const userNumber = document.querySelector('#guess').value;
+if (userNumber < 1 || userNumber > 20) {
+  Swal.fire({
+    icon: 'error',
+    title: 'Oops...! Something went wrong!',
+    text: 'Select number from 1 to 20',
+  })
+}
 }
